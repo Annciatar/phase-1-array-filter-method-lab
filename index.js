062af72d58f8418223b83cb97db8cd34257b270f
Annciatar Ndumi <annciatar.kavita@student.moringaschool.com>
2022-08-20 15:32:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(dereva, jina){
+    return dereva.filter(drivers => drivers.toUpperCase() === jina.toUpperCase())
+}
+function fuzzyMatch( dereva, jina){
+    return dereva.filter(function(user){
+        return user[0,1] === jina[0,1]
+    })
+}
+
+function matchName(dereva, jina){
+    return dereva.filter(function (user){
+        return user.name === jina;
+    })
+}
